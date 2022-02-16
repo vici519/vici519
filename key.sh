2603058a7b08fcfd8e7087ec4937b25309e64c1c
vici519 <admin@matveev.pro>
2022-02-16 10:13:27 +0300

    Create key.sh

---
+++ key.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mv /var/www/html/uploads/* /var/opt/cprocsp/keys/root
+sert=$(/opt/cprocsp/bin/amd64/csptest -keyset -enum_cont -verifycontext -fqcn | grep HDIMAGE)
+
+
+echo $sert
+/opt/cprocsp/bin/amd64/certmgr -inst -file /key/*.p7b -cont "$sert"
+/opt/cprocsp/bin/amd64/certmgr -list -store uMy > /opt/sert.txt
