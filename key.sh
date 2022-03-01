799cb102689fff3ad4cbc48acb7b98a473b325a2
vici519 <admin@matveev.pro>
2022-03-02 02:53:46 +0300

    Update key.sh

--- key.sh
+++ key.sh
@@ -3,6 +3,6 @@ mv /var/www/html/uploads/* /var/opt/cprocsp/keys/root
 sert=$(/opt/cprocsp/bin/amd64/csptest -keyset -enum_cont -verifycontext -fqcn | grep HDIMAGE)
 
 
-echo $sert
+#echo $sert
 /opt/cprocsp/bin/amd64/certmgr -inst -file /key/*.cer -cont "$sert"
 /opt/cprocsp/bin/amd64/certmgr -list -store uMy > /opt/sert.txt
